c668d6acc91e3e12b7321e3c510ceb5f79335be2
jtmacoco <jtmacoco@gmail.com>
2026-03-08 00:23:03 -0800

    update: added ncu report

--- benchmarks/gpu_bench.cu
+++ benchmarks/gpu_bench.cu
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
     Tokenizer tokenizer(vocab_path, merges_path);
 
     std::string input = "The quick brown fox jumps over the lazy dog and runs into the forest";
-    int tokens_to_generate = 500;
+    int tokens_to_generate = 2;
     if (argc > 1) tokens_to_generate = std::stoi(argv[1]);
 
     std::vector<int> tokens = tokenizer.Encoder(input);
